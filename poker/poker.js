18070e3518edd5ee7ede5d83028a57539d313a1f
Jason MacLean <macleajr@gmail.com>
2018-12-26 15:40:18 -0500

    Update poker.js

--- poker/poker.js
+++ poker/poker.js
@@ -1,44 +1,72 @@
 /* eslint-disable */
 
-var version = "0.0.9";
+var version = "0.1.0";
 var socket;
+
 $(document).ready(function () {
-    $("#status").append("Version " + version + "<br/>");
+    $("#status").prepend("Version " + version + "<br/>");
     $('#connect').click(function () {
-        $('#status').append("attempting to connect<br/>");
+        $('#status').prepend("attempting to connect<br/>");
         connectWs();
     });
 
 
     $(".vote").click(function () {
-        var payload = "{\"action\":\"vote\", \"method\": \"vote\", \"vote\":\"" + $(this).data("vote") + "\", \"name\": \"" + $("#name").val() + "\"}";
-        $('#status').append("sending payload: " + payload + "<br/>");
-        socket.send(payload);
+        var payload = new Object();
+        payload.action = "vote";
+        payload.method = "vote";
+        payload.vote = String($(this).data("vote"));
+        payload.name = $("#name").val();
+
+        $('#status').prepend("sending payload: " + JSON.stringify(payload) + "<br/>");
+        socket.send(JSON.stringify(payload));
     });
 
 });
+
+function processVotes(votes) {
+    $("#status").prepend("Processing votes<br/>");
+    $("#results").html("");
+    votes.forEach(function (v) {
+        console.log(v);
+
+        $("#results").prepend(v.name + " - " + v.vote + "<br/>");
+    });
+
+}
 
 function connectWs() {
     socket = new WebSocket('wss://gpb9s43yd2.execute-api.us-east-1.amazonaws.com/prod/');
     // Connection opened
     socket.addEventListener('open', function (event) {
-        $('#status').append("socket opened<br/>");
+        $('#status').prepend("socket opened<br/>");
         var payload = "{\"action\":\"register\", \"name\": \"" + $("#name").val() + "\"}";
-        $('#status').append("sending payload: " + payload + "<br/>");
+        $('#status').prepend("sending payload: " + payload + "<br/>");
         socket.send(payload);
     });
     // Listen for messages
     socket.addEventListener('message', function (event) {
-        $('#status').append(event.data + "<br/>");
+        try {
+            var message = JSON.parse(event.data);
+
+            if (message.votes) {
+                processVotes(message.votes);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+
+        $('#status').prepend(event.data + "<br/>");
+
     });
 
     socket.addEventListener('close', function (event) {
-        $('#status').append("socket closed<br/>");
+        $('#status').prepend("socket closed<br/>");
 
     });
 
     socket.onerror = function (e) {
-        $('#status').append("WebSocket Error: ", e);
+        $('#status').prepend("WebSocket Error: ", e);
         //Custom function for handling errors
 
     };
